a7ca686f31a487169f72fab74e41e48fa1dc962e
qowiys <qowiyshabir1416@gmail.com>
2025-10-22 18:27:43 +0700

    Menambahkan model review

--- apps/review/models.py
+++ apps/review/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.event.models import Event
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(default=0)
+    review_text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.event.name}"
