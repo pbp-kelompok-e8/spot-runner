27f98fad796c93e6101216b1a3570504c7611823
qowiys <qowiyshabir1416@gmail.com>
2025-10-24 23:08:38 +0700

    save progress buat dipush ke pws

--- spot_runner/settings.py
+++ spot_runner/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "william-jonnatan-spotrunner.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://pbp.cs.ui.ac.id/william.jonnatan/spotrunner",
+]
+
 
 # Application definition
 
