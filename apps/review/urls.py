dde9b37e436247e82fbbe351762221b7ffcd5391
HODEMASTER <peteryap0505@gmail.com>
2025-12-21 18:12:36 +0700

    merging with qowiy

--- apps/review/urls.py
+++ apps/review/urls.py
@@ -5,7 +5,11 @@ from . import views
 app_name = 'review'
 
 urlpatterns = [
+    path('api/reviews/', views.get_all_reviews, name='get_all_reviews'),
+    path('api/reviews/<str:review_id>/', views.get_review_detail, name='get_review_detail'),
+    path('api/reviews/event/<str:event_id>/', views.get_event_reviews, name='get_event_reviews'),
     path('create/<uuid:event_id>/', views.create_review, name='create_review'),
-    path('<int:review_id>/edit/', views.edit_review, name='edit_review'),
-    path('<int:review_id>/delete/', views.delete_review, name='delete_review'),
+    path('<str:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('<str:review_id>/delete/', views.delete_review, name='delete_review'),
+    path('create-flutter/', views.create_review_flutter, name='create_review_flutter'),
 ]
